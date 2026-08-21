6a0ee2fdd83d64a47d103737f6e0401f87452fa9
Jonathan <160444449+lElemental@users.noreply.github.com>
2026-08-21 01:35:06 +0000

    final

--- main.py
+++ main.py
@@ -1,3 +1,37 @@
 import operations
+import logging
 
-if __name__ == "__main__"
+logging.basicConfig(
+    level=logging.DEBUG,
+    filemode="w",
+    filename="test.log"
+    
+)
+
+if __name__ == "__main__":
+
+    ##logging.info('Test case 1')
+
+    ##result = operations.add(1,2)
+
+    ##if result == 3:
+      ##  logging.info("PASS")
+
+   ## else :
+     ##   logging.info("FAIL")
+
+    logging.info('Test case 3')
+    result = operations.power(2,3)
+    if result == 1:
+        logging.info("PASS")
+    else :
+        logging.info("FAIL")
+
+
+    ##result = operations.add(3,2)
+    logging.info(f"Result= {result}")
+
+    logging.warning("Division endiablada")
+    logging.error('Error')
+    logging.debug('The value in a and b is ...')
+    logging.critical('Critical in time')
